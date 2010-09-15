3e67f8323a433faf704d44fd86796591025eb8d6
Liu Qun <liuqun@email.xldz.com>
2010-09-16 01:31:16 +0800

    为编译控制台程序重写hello.mk

--- hello.mk
+++ hello.mk
@@ -1,5 +1,13 @@
-execultable_files += hello
-source_files  += hello.c
-object_files  += hello.o
-hello: hello.o
-hello.o: hello.c
+local_execultable_file_basename := hello
+local_execultable_file := $(addsuffix $(EXEEXT), $(local_execultable_file_basename))
+local_source_files :=
+local_source_files += hello.c
+local_object_files := $(local_source_files:.c=.o)
+
+source_files += $(local_source_files)
+object_files += $(local_object_files)
+execultable_files += $(local_execultable_file)
+
+$(local_execultable_file): $(local_object_files)
+	$(LINK.o) $^ $(LDLIBS) -o $@
+$(local_execultable_file): LDLIBS=
